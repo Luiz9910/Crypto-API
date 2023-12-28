25341a4a358910ae97d961daf00cceb8c39aaa3c
Luiz Felipe <luizfelipealvesdesouza706@gmail.com>
2023-12-27 22:48:36 -0300

    sistema para pegar lista de transações de uma unica crypto por nome concluido, também foi feito o sistema de deleção

---
+++ src/main/java/com/example/crypto/controller/CoinController.java
@@ -0,0 +1,87 @@
+package com.example.crypto.controller;
+
+import com.example.crypto.entity.Coin;
+import com.example.crypto.repository.CoinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping("/coin")
+public class CoinController {
+    @Autowired
+    private CoinRepository coinRepository;
+
+    @Bean
+    public Coin init() {
+       Coin c1 = new Coin();
+       c1.setName("BITCOIN");
+       c1.setPrice(new BigDecimal(100));
+       c1.setQuantity(new BigDecimal(0.00003));
+       c1.setDateTime(new Timestamp(System.currentTimeMillis()));
+
+        Coin c2 = new Coin();
+        c2.setName("BITCOIN");
+        c2.setPrice(new BigDecimal(100));
+        c2.setQuantity(new BigDecimal(0.00003));
+        c2.setDateTime(new Timestamp(System.currentTimeMillis()));
+
+        Coin c3 = new Coin();
+        c3.setName("ETHEREUM");
+        c3.setPrice(new BigDecimal(100));
+        c3.setQuantity(new BigDecimal(0.00003));
+        c3.setDateTime(new Timestamp(System.currentTimeMillis()));
+
+
+        coinRepository.insert(c1);
+        coinRepository.insert(c2);
+        coinRepository.insert(c3);
+
+        return c1;
+    }
+
+    @GetMapping
+    public ResponseEntity getAll() {
+        try {
+            return new ResponseEntity<>(coinRepository.getAll(), HttpStatus.OK);
+        } catch (Exception error) {
+            return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity getAllByName(@PathVariable String name) {
+        try {
+            return new ResponseEntity<>(coinRepository.getByName(name), HttpStatus.OK);
+        } catch (Exception error) {
+            return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity post(@RequestBody Coin coin) {
+        try {
+            coin.setDateTime(new Timestamp(System.currentTimeMillis()));
+            System.out.println(coin.getDateTime());
+            return new ResponseEntity<>(coinRepository.insert(coin), HttpStatus.CREATED);
+        } catch (Exception error) {
+            return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity destroy(@PathVariable Integer id) {
+        try {
+            return new ResponseEntity<>(coinRepository.remove(id) ,HttpStatus.OK);
+        } catch (Exception error){
+            return new ResponseEntity<>(error.getMessage(), HttpStatus.NO_CONTENT);
+        }
+    }
+}
