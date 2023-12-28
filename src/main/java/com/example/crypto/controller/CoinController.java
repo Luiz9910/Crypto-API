1d7cff47b7c4a8e29e96e4d8d7be799664c570c8
Luiz Felipe <luizfelipealvesdesouza706@gmail.com>
2023-12-28 18:53:34 -0300

    controller init removido

--- src/main/java/com/example/crypto/controller/CoinController.java
+++ src/main/java/com/example/crypto/controller/CoinController.java
@@ -17,34 +17,6 @@ public class CoinController {
     @Autowired
     private CoinRepository coinRepository;
 
-    @Bean
-    public Coin init() {
-       Coin c1 = new Coin();
-       c1.setName("BITCOIN");
-       c1.setPrice(new BigDecimal(100));
-       c1.setQuantity(new BigDecimal(0.00003));
-       c1.setDateTime(new Timestamp(System.currentTimeMillis()));
-
-        Coin c2 = new Coin();
-        c2.setName("BITCOIN");
-        c2.setPrice(new BigDecimal(100));
-        c2.setQuantity(new BigDecimal(0.00003));
-        c2.setDateTime(new Timestamp(System.currentTimeMillis()));
-
-        Coin c3 = new Coin();
-        c3.setName("ETHEREUM");
-        c3.setPrice(new BigDecimal(100));
-        c3.setQuantity(new BigDecimal(0.00003));
-        c3.setDateTime(new Timestamp(System.currentTimeMillis()));
-
-
-        coinRepository.insert(c1);
-        coinRepository.insert(c2);
-        coinRepository.insert(c3);
-
-        return c1;
-    }
-
     @GetMapping
     public ResponseEntity getAll() {
         try {
